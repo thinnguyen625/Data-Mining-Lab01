ec0dedf7c4ec04fa3c239b0b8bad3f69d2ab23dc
EricNguyen99 <vanthin7111999@gmail.com>
2019-10-05 17:07:21 +0700

    + chuyen file txt sang csv

--- 19_BT2/19_BT2.py
+++ 19_BT2/19_BT2.py
@@ -1,19 +1,46 @@
 import sys
-import os
+import parser
+import csv
+
+
+def process_file_text():
+    with open("countries.txt", "r",encoding="utf8") as fi:
+        with open("output.csv", 'w',encoding="utf8",newline='') as writeFile:
+            csv_writer = csv.writer(writeFile)
+            i = 0
+            t = 0
+            result = []
+            data = []
+            while True:
+                line = fi.readline()
+                if line == '' or line == None:
+                    break
+                if i < 8:
+                    temp = line.strip('\n')
+                    temp = temp.split('=')
+                    result.append(temp[0])
+                    if i == 7:
+                        csv_writer.writerow(result)
+                else:
+                    temp = line.strip('\n')
+                    temp = temp.split('=')
+                    if temp[0] == result[0]:
+                        t+=1
+                        if t == 2:
+                            csv_writer.writerow(data)
+                            t=1
+                        data = [temp[1],'','','','','','','']    
+                    else:
+                        tempIndex = result.index(temp[0])
+                        data[tempIndex] = temp[1]                   
+                i += 1
+            writeFile.close()
+    fi.close()
 
 def main():
-    inputfile = "countries.txt"
-    outputfile = "output.txt"
-    with open(outputfile, "r") as f:
-        lines = f.readlines()
-        print(lines)
-    with open(outputfile, "w") as f:
-        for line in lines:
-            string = line.strip("\n")
-            if string != "name=name":
-                    f.write(line)
-
+    process_file_text()
 
 if __name__ == '__main__':
     main()
-        
+    
+
